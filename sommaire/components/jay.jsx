23a3d96d5ec3a2f6a0a2293d3117cfdcf6e8cc5c
JAYSINGH <mail.jaynamdhari@gmail.com>
2025-04-19 01:10:50 +0530

    Co-authored-by: kingsimran <kingsimran@users.noreply.github.com>

---
+++ sommaire/components/jay.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const jay = () => {
+  return (
+    <div>jay i writing the commits int the file</div>
+  )
+}
+
+export default 
---
+++ sommaire/components/navbar/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import styles from "./navbar.module.css";
+
+
+export default Navabar = () =>{
+    return(
+        <div className={styles.outer}>
+            
+            
+
+        </div>
+
+    );
+}
